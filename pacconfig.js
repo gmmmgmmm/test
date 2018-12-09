26993112d0634a346e741c37be96c08650df1753
gm <45603753+gmmmgmmm@users.noreply.github.com>
2018-12-10 00:17:39 +0800

    Update pacconfig.js

--- pacconfig.js
+++ pacconfig.js
@@ -2,28 +2,27 @@ function FindProxyForURL(url, host) {
   var porxyindex = 0;
   var proxy_direct = "DIRECT;";
   var proxy_yes = [
-    "SOCKS5 5.56.133.221:1080;",    
-    "SOCKS5 50.62.35.81:62871;",
-    "SOCKS5 192.169.140.74:47797;",
-    "SOCKS5 50.62.35.225:59537;",
-    "SOCKS5 13.81.169.254:1080;", 
-    "SOCKS5 59.115.36.182:1080;",//5
-    "SOCKS5 132.148.130.46:33324;",
-    "SOCKS5 132.148.130.46:48684;",
-    "SOCKS5 192.169.218.61:59670;",
-    "SOCKS5 104.238.97.230:48829;",
-    "SOCKS5 192.169.232.12:62194;",//10
-    "SOCKS5 192.169.140.74:47797;",
-    "SOCKS5 132.148.130.46:14945;",
-    "SOCKS5 132.148.130.46:30900;",
-    "SOCKS5 132.148.130.46:36939",
-    "SOCKS5 132.148.130.46:51404;",//15
-    "SOCKS5 192.169.140.74:52210;",
-    "SOCKS5 13.81.169.254:1080;",
-    "SOCKS5 162.243.210.52:16021;",
-    "SOCKS5 192.151.146.130:45376;"//fr 9m
+    "SOCKS5 5.56.133.221:1080;"
+    ,"SOCKS5 192.169.232.12:62194;"
+    ,"SOCKS5 132.148.130.46:48684;"
+    ,"SOCKS5 132.148.130.46:36939;"
+    ,"SOCKS5 132.148.130.46:30900;"
+    ,"SOCKS5 50.62.35.81:62871;"//5
+    ,"SOCKS5 192.169.140.74:47797;"
+    ,"SOCKS5 132.148.130.46:33324;"
+    ,"SOCKS5 192.169.140.74:52210;"
+    ,"SOCKS5 132.148.130.46:14945;"
+    ,"SOCKS5 192.169.189.215:44786;"//10
+    ,"SOCKS5 132.148.130.46:51404;"
+    ,"SOCKS5 13.81.169.254:1080;"
+    ,"SOCKS5 5.56.133.221:1080;"
+    ,"SOCKS5 66.84.13.139:53238;"//rus 9m
+    ,"SOCKS5 59.115.44.71:1080;"//15 tw
+    
+    ,"SOCKS5 166.62.59.175:37954"
   ];
   /*
+
     */
   var proxy_yeslist = ["45.32.164.128", "*google*", "*goo.gl*", "*gmail*", "*youtube*", "*ytimg.com*",
     "*chrome.com*", "*android.com*", "*ggpht.com*", "*blogger.com*", "*wordpress.com*", "*facebook.com*",
@@ -33,8 +32,10 @@ function FindProxyForURL(url, host) {
   var proxy_directlist = [
     "*baidu.com*", "*qq.com*", "ip111.cn", "*csdn.net*", "*proxyscrape.com*",
     "*toutiao.com*", "*tianya.cn*", "*dichvusocks.us*", "*github.com*",
-    "*githubusercontent*", "*cnblogs.com*"
+    "*githubusercontent*", "*cnblogs.com*","*tianyaui.com*"
+    ,"*pstatp.com*","*.huanqiu.*"
   ];
+  
   for (var i = 0; i <
     proxy_directlist.length; i++) {
     if (shExpMatch(host, proxy_directlist[i]))
@@ -56,10 +57,11 @@ if  ((m & 1) === 0)
   porxyindex = 13;
   }
   else
-  { porxyindex = 14; };
+  { porxyindex = 19; };
   
   */
-  porxyindex = 19;
+ porxyindex = 11;
+//   porxyindex = 15;
   return proxy_yes[porxyindex]
 }
 
