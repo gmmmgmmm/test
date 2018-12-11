16a5d0ea7d67429787522bbb9966cc1b1b458d48
gm <45603753+gmmmgmmm@users.noreply.github.com>
2018-12-12 00:42:55 +0800

    Update pacconfig.js

--- pacconfig.js
+++ pacconfig.js
@@ -18,18 +18,16 @@ function FindProxyForURL(url, host) {
     ,"SOCKS5 5.56.133.221:1080;"
     ,"SOCKS5 35.225.214.70:9581;"//egypt 9m
     ,"SOCKS5 107.150.8.13:1080;"//15 10234k
-    
-    ,"SOCKS5 166.62.59.175:37954"
-    ,"SOCKS5 122.116.161.100:30114"
+   
+    ,"SOCKS5 167.179.88.29:1080;"//jp
+    ,"SOCKS5 52.192.76.155:1080;"//jp
+    ,"SOCKS5 122.116.161.100:30114"//tw
     ,"SOCKS5 50.62.35.162:43185"
-    ,"SOCKS5 104.238.97.230:48829"
-    ,"SOCKS5 163.172.166.20:4480"//20
+    ,"SOCKS5 104.238.97.230:48829"//20
     ,"SOCKS5 51.158.79.152:1080;"//fr
-    ,"SOCKS5 35.193.155.198:9797;"//albaba
-    ,"SOCKS5 148.251.160.200:4145;"//germany
     ,"SOCKS5 163.172.166.20:4480;"//france
-    ,"SOCKS5 166.62.84.121:51426;"
-    ,"SOCKS5 50.63.167.72:61480;"
+    ,"SOCKS5 59.115.62.225:1080;"//tw
+    ,"SOCKS5 37.97.190.42:8080;"//nl
   ];
   /*
 
@@ -40,10 +38,12 @@ function FindProxyForURL(url, host) {
     "*blogblog.com*", "dist.github.com*", "*.tensorflow.org*", "*shadowsocks.org*"
   ];
   var proxy_directlist = [
-    "*baidu.com*", "*qq.com*", "ip111.cn", "*csdn.net*", "*proxyscrape.com*",
-    "*toutiao.com*", "*tianya.cn*", "*dichvusocks.us*", "*github.com*",
+    "*baidu.com*", "*qq.com*", "ip111.cn", "*csdn.net*"
+    , "*proxyscrape.com*","*toutiao.com*", "*tianya.cn*"
+    , "*dichvusocks.us*","*checksocks5.com*"
+    , "*github.com*",
     "*githubusercontent*", "*cnblogs.com*","*tianyaui.com*"
-    ,"*pstatp.com*","*.huanqiu.*"
+    ,"*pstatp.com*","*.huanqiu.*","*guancha.cn*"
   ];
   
   for (var i = 0; i <
@@ -70,7 +70,9 @@ if  ((m & 1) === 0)
   { porxyindex = 19; };
   
   */
-   porxyindex = 21;
-//   porxyindex = 19;
+   porxyindex = 24;
+   porxyindex = 23;
   return proxy_yes[porxyindex]
 }
+
+
